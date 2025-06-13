3850f426dfa1744e4c03f0b87e64d7fa6b16e6f0
David Cheung <davxgnu@tamu.edu>
2025-06-13 02:53:59 -0500

    MOD main.js MOD style.css
    Added boilerplate code

--- cube-event-test-1/src/main.js
+++ cube-event-test-1/src/main.js
@@ -3,5 +3,21 @@ import {
 } from './util-three-1';
 import * as THREE from 'three';
 
-let { camera, scene, renderer } = createScene();
-console.log(camera, scene, renderer);
+let { scene, camera, renderer } = createScene();
+// console.log(scene, camera, renderer);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  renderer.render( scene, camera );
+  cube.rotateX(0.05);
+}
+renderer.setAnimationLoop( animate );
+
+
+document.body.appendChild(renderer.domElement);
