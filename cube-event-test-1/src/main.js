97b9a9ff7ca16dec6e3ca2465c044049e85c9a4d
David Cheung <davxgnu@tamu.edu>
2025-06-18 16:16:03 -0500

    MOD util-three-1.js, main.js, style.css
    Added utility function to create THREE.js cube.
    Currently 1 cube rendered. This marks the start of multiple cube event testing

--- cube-event-test-1/src/main.js
+++ cube-event-test-1/src/main.js
@@ -1,23 +1,41 @@
 import {
-  createScene
+  createScene,
+  createCube
 } from './util-three-1';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 let { scene, camera, renderer } = createScene();
-// console.log(scene, camera, renderer);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+document.body.appendChild(renderer.domElement);
+const color = new THREE.Color(0x12121f);
+scene.background = color
 camera.position.z = 5;
+
+const controls = new OrbitControls(camera, document.body);
+controls.target.set(0, 0, 0);  // pt of focus for the orbit
+controls.update();
+
+const cube1 = createCube([1, 1, 1], '0x00ff00', [0, 0, 0])
+scene.add( cube1 );
+
 
 function animate() {
   renderer.render( scene, camera );
-  cube.rotateX(0.05);
 }
 renderer.setAnimationLoop( animate );
 
+window.addEventListener('keydown', function(event){
+  const userInput = event.key.toLowerCase();
+  if ('e'.includes(userInput)) {
+    cube1.translateY(1);
+  }
+});
 
-document.body.appendChild(renderer.domElement);
+window.addEventListener('keyup', function(event){
+  const userInput = event.key.toLowerCase();
+  if ('e'.includes(userInput)) {
+    cube1.translateY(-1);
+  }
+});
+
