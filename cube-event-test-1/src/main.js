5254f03f55336de60d5dc5b92c2d0062e4d40a32
David Cheung <davxgnu@tamu.edu>
2025-06-18 22:41:37 -0500

    MOD main.js
    Successfuly render 7 cube (our keycaps) that respond to user input with translation across y-axis

--- cube-event-test-1/src/main.js
+++ cube-event-test-1/src/main.js
@@ -5,6 +5,7 @@ import {
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
+// Set up Scene Environment Items
 let { scene, camera, renderer } = createScene();
 
 document.body.appendChild(renderer.domElement);
@@ -12,35 +13,54 @@ const color = new THREE.Color(0x12121f);
 scene.background = color
 camera.position.z = 5;
 
+// Orbit Controls (optional)
 const controls = new OrbitControls(camera, document.body);
 controls.target.set(0, 0, 0);  // pt of focus for the orbit
 controls.update();
 
-const cube1 = createCube([1, 1, 1], 0xff0000, [0, 0, 0])
-const cube2 = createCube([1, 1, 1], 0x00ff00, [1.5, 0, 0])
-const cube3 = createCube([1, 1, 1], 0x0000ff, [3, 0, 0])
-scene.add( cube1, cube2, cube3 );
+//  Create Cubes
+const cubeA = createCube([1, 1, 1], 0xff0000, [-4.5, 0, 0])
+const cubeB = createCube([1, 1, 1], 0xff9f00, [-3, 0, 0])
+const cubeC = createCube([1, 1, 1], 0xffff00, [-1.5, 0, 0])
+const cubeD = createCube([1, 1, 1], 0x00ff00, [0, 0, 0])
+const cubeE = createCube([1, 1, 1], 0x0000ff, [1.5, 0, 0])
+const cubeF = createCube([1, 1, 1], 0x9f00ff, [3, 0, 0])
+const cubeG = createCube([1, 1, 1], 0xf900ff, [4.5, 0, 0])
+scene.add( cubeA, cubeB, cubeC, cubeD, cubeE, cubeF, cubeG );
 
-
+//  Animation Loop
 function animate() {
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
 
-let keyDown = false
+let keyA, keyB, keyC, keyD, keyE, keyF, keyG = false;
+const cubeDict = {
+  A: [cubeA, keyA],
+  B: [cubeB, keyB],
+  C: [cubeC, keyC],
+  D: [cubeD, keyD],
+  E: [cubeE, keyE],
+  F: [cubeF, keyF],
+  G: [cubeG, keyG]
+};
+
 window.addEventListener('keydown', function(event){
   const userInput = event.key.toLowerCase();
-  if (('e'.includes(userInput)) && (!keyDown)) {
-    cube1.translateY(-0.5);
-    keyDown = true
+  if (('abcdefg'.includes(userInput.toLowerCase()))) {
+    if (!cubeDict[userInput.toUpperCase()][1]) {
+      (cubeDict[userInput.toUpperCase()][0]).translateY(-0.5);
+      cubeDict[userInput.toUpperCase()][1] = true;
+    }
   }
 });
 
 window.addEventListener('keyup', function(event){
   const userInput = event.key.toLowerCase();
-  if ('e'.includes(userInput)) {
-    cube1.translateY(0.5);
-    keyDown = false
+  if (('abcdefg'.includes(userInput.toLowerCase()))) {
+    (cubeDict[userInput.toUpperCase()][0]).translateY(0.5);
+    cubeDict[userInput.toUpperCase()][1] = false;
+    
   }
 });
 
