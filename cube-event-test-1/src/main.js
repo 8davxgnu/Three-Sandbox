9cca3e7bc1669c02ef9c6fa4eee6454aea142946
David Cheung <davxgnu@tamu.edu>
2025-06-18 19:54:47 -0500

    MOD main.js
    Add lock boolean to keydown event to prevent continuous event trigger

--- cube-event-test-1/src/main.js
+++ cube-event-test-1/src/main.js
@@ -16,8 +16,10 @@ const controls = new OrbitControls(camera, document.body);
 controls.target.set(0, 0, 0);  // pt of focus for the orbit
 controls.update();
 
-const cube1 = createCube([1, 1, 1], 0x00ff00, [0, 0, 0])
-scene.add( cube1 );
+const cube1 = createCube([1, 1, 1], 0xff0000, [0, 0, 0])
+const cube2 = createCube([1, 1, 1], 0x00ff00, [1.5, 0, 0])
+const cube3 = createCube([1, 1, 1], 0x0000ff, [3, 0, 0])
+scene.add( cube1, cube2, cube3 );
 
 
 function animate() {
@@ -25,17 +27,20 @@ function animate() {
 }
 renderer.setAnimationLoop( animate );
 
+let keyDown = false
 window.addEventListener('keydown', function(event){
   const userInput = event.key.toLowerCase();
-  if ('e'.includes(userInput)) {
-    cube1.translateY(1);
+  if (('e'.includes(userInput)) && (!keyDown)) {
+    cube1.translateY(-0.5);
+    keyDown = true
   }
 });
 
 window.addEventListener('keyup', function(event){
   const userInput = event.key.toLowerCase();
   if ('e'.includes(userInput)) {
-    cube1.translateY(-1);
+    cube1.translateY(0.5);
+    keyDown = false
   }
 });
 
