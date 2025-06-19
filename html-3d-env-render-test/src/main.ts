23c959a23d39823d4eb60992e30c783ebc4d2167
David Cheung <davxgnu@tamu.edu>
2025-06-19 03:53:48 -0500

    MOD main.ts, style.css, util.ts
    Adding placeholder three.js render from cube test proj to ensure three is installed correctly with typescript

--- html-3d-env-render-test/src/main.ts
+++ html-3d-env-render-test/src/main.ts
@@ -1,24 +1,77 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
     <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import {
+  createScene,
+  createCube
+} from './util';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+
+// Set up Scene Environment Items
+let { scene, camera, renderer } = createScene();
+
+document.body.appendChild(renderer.domElement);
+const color = new THREE.Color(0x12121f);
+scene.background = color
+camera.position.z = 5;
+
+// Orbit Controls (optional)
+const controls = new OrbitControls(camera, document.body);
+controls.target.set(0, 0, 0);  // pt of focus for the orbit
+controls.update();
+
+//  Create Cubes
+const cubeA = createCube([1, 1, 1], 0xff0000, [-4.5, 0, 0])
+const cubeB = createCube([1, 1, 1], 0xff9f00, [-3, 0, 0])
+const cubeC = createCube([1, 1, 1], 0xffff00, [-1.5, 0, 0])
+const cubeD = createCube([1, 1, 1], 0x00ff00, [0, 0, 0])
+const cubeE = createCube([1, 1, 1], 0x0000ff, [1.5, 0, 0])
+const cubeF = createCube([1, 1, 1], 0x9f00ff, [3, 0, 0])
+const cubeG = createCube([1, 1, 1], 0xf900ff, [4.5, 0, 0])
+scene.add( cubeA, cubeB, cubeC, cubeD, cubeE, cubeF, cubeG );
+
+//  Animation Loop
+function animate() {
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
+let keyA, keyB, keyC, keyD, keyE, keyF, keyG = false;
+const cubeDict = {
+  A: [cubeA, keyA],
+  B: [cubeB, keyB],
+  C: [cubeC, keyC],
+  D: [cubeD, keyD],
+  E: [cubeE, keyE],
+  F: [cubeF, keyF],
+  G: [cubeG, keyG]
+};
+
+window.addEventListener('keydown', function(event){
+  const userInput = event.key.toLowerCase();
+  if (('abcdefg'.includes(userInput.toLowerCase()))) {
+    if (!cubeDict[userInput.toUpperCase()][1]) {
+      (cubeDict[userInput.toUpperCase()][0]).translateY(-0.5);
+      cubeDict[userInput.toUpperCase()][1] = true;
+    }
+  }
+});
+
+window.addEventListener('keyup', function(event){
+  const userInput = event.key.toLowerCase();
+  if (('abcdefg'.includes(userInput.toLowerCase()))) {
+    (cubeDict[userInput.toUpperCase()][0]).translateY(0.5);
+    cubeDict[userInput.toUpperCase()][1] = false;
+    
+  }
+});
+
---
+++ html-3d-env-render-test/src/util.ts
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+
+export function createScene() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE. WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    return { scene, camera, renderer };
+}
+
+export function createCube([a,b,c]: number[], color: number, [x, y, z]: number[]) {
+    const geometry = new THREE.BoxGeometry( a, b, c );
+    const material = new THREE.MeshBasicMaterial({color: color});
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.set(x, y, z)
+    return cube
+}
